73ab0e98d43902f744c823395f6b043c7a00b782
Artem Dimoglov <adimoglov@gmail.com>
2025-02-12 11:50:30 +0200

    icons

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,8 +1,15 @@
+import {FaTelegram, FaPhone} from "react-icons/fa6";
+
 export const Footer = () => {
     return (
         <footer className='absolute z-[90] items-center bottom-3 left-1/2 transform -translate-x-1/2 flex'>
-            <div className='flex px-4 py-0.5 items-center'>
-                <a href={`tel:${+380730672127}`} className=''>+380730672127</a>
+            <div className='flex gap-x-2 py-0.5 items-center'>
+                <a href="https://t.me/+380730672127" target="_blank" rel="noopener noreferrer" className=''>
+                    <FaTelegram className='text-[26px] text-primary'/>
+                </a>
+                <a href={`tel:${+380730672127}`} className=''>
+                    <FaPhone className='text-[26px] text-primary'/>
+                </a>
             </div>
         </footer>
     )
