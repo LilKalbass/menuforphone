bec9cfeec113b2c0f658a67eb2139ab02a726da2
Artem Dimoglov <adimoglov@gmail.com>
2025-02-13 01:33:18 +0200

    tryout

--- app/layout.js
+++ app/layout.js
@@ -31,9 +31,8 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
     <head>
-      <Script
+      <script
           src="https://telegram.org/js/telegram-web-app.js"
-          strategy="beforeInteractive"
       />
     </head>
       <body
