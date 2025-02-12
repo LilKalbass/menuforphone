ab71fe969d0740c3709c5bb409ed4c36698a7200
Artem Dimoglov <adimoglov@gmail.com>
2025-02-13 01:29:28 +0200

    tryout

--- app/layout.js
+++ app/layout.js
@@ -2,7 +2,6 @@ import {PT_Serif, Forum, Exo_2 } from "next/font/google";
 import "./globals.css";
 import {Header} from "../components/Header";
 import {Footer} from "../components/Footer";
-import Head from "next/head";
 import Script from "next/script";
 
 const ptSerif = PT_Serif({
@@ -31,13 +30,12 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-    <Head>
+    <head>
       <Script
           src="https://telegram.org/js/telegram-web-app.js"
           strategy="beforeInteractive"
-          onLoad={() => console.log("Telegram script загружен")}
       />
-    </Head>
+    </head>
       <body
         className={`${ptSerif.variable} ${forum.variable} ${exo.variable} antialiased transition-all`}
       >
