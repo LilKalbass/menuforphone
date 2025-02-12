c76a41f0f9d7776d9a04510cec2e453326f7186d
Artem Dimoglov <adimoglov@gmail.com>
2025-02-12 10:56:55 +0200

    f

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,7 +1,7 @@
 export const Footer = () => {
     return (
         <footer className='absolute z-[90] items-center bottom-3 left-1/2 transform -translate-x-1/2 flex'>
-            <div className='flex border rounded-full border-black px-4 py-0.5 items-center'>
+            <div className='flex px-4 py-0.5 items-center'>
                 <a href={`tel:${+380730672127}`} className=''>+380730672127</a>
             </div>
         </footer>
