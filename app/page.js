c5e02f8b0552de3b26a94c98ab7dd71efcfafa25
Artem Dimoglov <adimoglov@gmail.com>
2025-02-13 00:16:57 +0200

    fix

--- app/page.js
+++ app/page.js
@@ -1,20 +1,29 @@
+'use client'
+
+import { useEffect } from "react";
 import Slider from "@/components/Slider";
 import Head from "next/head";
 
 export default function Home() {
-  return (
-      <>
-          <Head>
-              <script
-                  async
-                  type="text/javascript"
-                  src="https://telegram.org/js/telegram-web-app.js"
-              >
-              </script>
-          </Head>
-          <main className='h-screen'>
-              <Slider/>
-          </main>
-      </>
-  );
+    useEffect(() => {
+        // Проверка, что объект Telegram WebApp доступен
+        if (window.Telegram && window.Telegram.WebApp) {
+            window.Telegram.WebApp.init();
+        }
+    }, []);
+
+    return (
+        <>
+            <Head>
+                <script
+                    async
+                    type="text/javascript"
+                    src="https://telegram.org/js/telegram-web-app.js"
+                ></script>
+            </Head>
+            <main className="h-screen">
+                <Slider />
+            </main>
+        </>
+    );
 }
