5eb26ee141ee738d9781f7fd501172cff43fbab0
Artem Dimoglov <adimoglov@gmail.com>
2025-02-13 01:00:25 +0200

    fix

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,7 @@
 import {PT_Serif, Forum, Exo_2 } from "next/font/google";
 import "./globals.css";
-import {Header} from "@/components/Header";
-import {Footer} from "@/components/Footer";
+import {Header} from "../components/Header";
+import {Footer} from "../components/Footer";
 
 const ptSerif = PT_Serif({
   variable: "--font-pt-serif",
