34519091ff8bed07042b3bf5dde2c29da1ab070d
Artem Dimoglov <adimoglov@gmail.com>
2025-02-13 01:21:31 +0200

    fix

--- app/page.js
+++ app/page.js
@@ -1,21 +1,11 @@
-'use client'
-
-import { useEffect, useState } from "react";
 import Slider from "../components/Slider";
 
 export default function Home() {
-    const [isTelegramWebApp, setIsTelegramWebApp] = useState(false);
 
-    useEffect(() => {
-        if (typeof window !== "undefined" && window.Telegram?.WebApp) {
-            setIsTelegramWebApp(true);
-            window.Telegram.WebApp.init();
-        }
-    }, []);
 
     return (
         <main className="h-screen">
-            {isTelegramWebApp ? <Slider /> : <p>Пожалуйста, откройте в Telegram WebApp</p>}
+            <Slider />
         </main>
     );
 }
