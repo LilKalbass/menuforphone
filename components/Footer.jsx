aaf71689763b9b1b63be0645c11f5e7bf840fced
Artem Dimoglov <adimoglov@gmail.com>
2025-02-13 05:04:57 +0200

    qwe

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -2,7 +2,7 @@ import {FaTelegram} from "react-icons/fa6";
 
 export const Footer = () => {
     return (
-        <footer className='absolute z-[90] items-center bottom-3 left-1/2 transform -translate-x-1/2 flex'>
+        <footer className='absolute z-[90] md:z-0  items-center bottom-3 left-1/2 transform -translate-x-1/2 flex'>
             <div className='flex py-0.5 items-center'>
                 <a href="https://t.me/SenonKray" target="_blank" rel="noopener noreferrer" className=''>
                     <FaTelegram className='text-[26px] '/>
