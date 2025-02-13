803c5f8a9a79789313a0fa80e1ba6610b1f85e53
Artem Dimoglov <adimoglov@gmail.com>
2025-02-13 04:44:08 +0200

    qwe

--- app/layout.js
+++ app/layout.js
@@ -32,8 +32,8 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en" >
     <head>
-      <script
-          src="https://telegram.org/js/telegram-web-app.js"
+      <Script
+          src="https://telegram.org/js/telegram-web-app.js" strategy="beforeInteractive"
       />
     </head>
       <body
--- app/page.js
+++ app/page.js
@@ -1,6 +1,21 @@
+'use client'
+
+import { useEffect, useState } from "react";
 import Slider from "../components/Slider";
+import WebApp from "@twa-dev/sdk";
 
 export default function Home() {
+    const [isTelegram, setIsTelegram] = useState(false);
+
+    useEffect(() => {
+        if (typeof window !== "undefined" && window.Telegram?.WebApp) {
+            WebApp.ready(); // Инициализация WebApp SDK
+            setIsTelegram(true);
+        }
+    }, []);
+
+    if (!isTelegram) return null; // Не рендерим вне Telegram WebApp
+
     return (
         <main className="h-screen">
             <Slider />
